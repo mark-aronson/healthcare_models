38024eb6432b8b0998a78e5e1887a40e9b71bf92
mark-aronson <marksaronson@gmail.com>
2024-01-10 13:30:54 -0500

    fix data pull using API

--- medicaidData.R
+++ medicaidData.R
@@ -1,7 +1,19 @@
 library(httr)
 
+
 response <- GET("https://data.cms.gov/data-api/v1/dataset/be64fce3-e835-4589-b46b-024198e524a6/data")
-data <- content(response,as="parsed")
+rawData <- content(response,as="parsed")
 
-print(data[1])
+# get sample for preallocation 
+sampleData <- data.frame(rawData[1])
+frame_colnames <- colnames(sampleData)
+data <- data.frame(matrix(ncol=length(frame_colnames),nrow=length(rawData)))
+colnames(data) <- frame_colnames
 
+# iterate and fill data frame 
+for (i in 1:length(rawData)) {
+  data[i,] <- data.frame(rawData[i])
+}
+
+# view data
+head(data)
