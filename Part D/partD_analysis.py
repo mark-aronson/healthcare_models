07e96d51df25b5c2b2545a61d80e290932068eb8
mark-aronson <marksaronson@gmail.com>
2025-02-17 21:03:15 -0500

    fix racial data function

--- Part D/partD_analysis.py
+++ Part D/partD_analysis.py
@@ -11,6 +11,8 @@ opioid_data = pd.read_csv('Medicare_Part_D_Opioid_Prescribing_Rates_by_Geography
 # Add leading zeros to ensure all entries have five characters
 opioid_data['GEOID'] = opioid_data['Prscrbr_Geo_Cd'].apply(lambda x: str(x).zfill(5))
 
+#%% plot choropleth of opioid perscription rate 
+
 shapefile_path_counties_2022 = 'tl_2022_us_county/tl_2022_us_county.shp'
 
 shapefile = fun.merge_shapefile_data(shapefile_path_counties_2022,opioid_data,'GEOID','Opioid_Prscrbng_Rate')
@@ -26,22 +28,17 @@ acs_demographic_housing_data['GEOID'] = acs_demographic_housing_data['GEO_ID'].s
 
 #%% Extract demographic groups 
 
+age_data = fun.extract_age_data(acs_demographic_housing_data)
+race_data = fun.extract_race_data(acs_demographic_housing_data)
+
 # codebook: https://api.census.gov/data/2018/acs/acs5/profile/groups/DP05.html
 
-variables = ['DP05_0005PE','DP05_0006PE','DP05_0007PE','DP05_0008PE',
-             'DP05_0009PE','DP05_0010PE','DP05_0011PE','DP05_0012PE',
-             'DP05_0013PE','DP05_0014PE','DP05_0015PE','DP05_0016PE',
-             'DP05_0017PE']
-labels = ['under 5','5-9','10-14','15-19','20-24','25-34','35-44','45-54',
-          '55-59','60-64','65-74','75-84','85+']
-
-age_data = acs_demographic_housing_data[variables]
-age_data.columns = labels
 
 #%% start building dataset 
 
 data = opioid_data[['GEOID','Opioid_Prscrbng_Rate']]
 
-data = pd.merge(data,acs_demographic_housing_data[['DP05_0001E','GEOID']],on='GEOID',how='left')
+data = pd.merge(data,age_data,on='GEOID',how='left')
+data = pd.merge(data,race_data,on='GEOID',how='left')
 
 
--- Part D/partD_analysis_func.py
+++ Part D/partD_analysis_func.py
@@ -40,6 +40,47 @@ def merge_shapefile_data(shapefile_path, dataframe, merge_column, data_column):
     except Exception as e:
         print(f"Error: {e}")
 
+def extract_age_data(data):
+    
+    variables = ['GEOID','DP05_0005PE','DP05_0006PE','DP05_0007PE','DP05_0008PE',
+                 'DP05_0009PE','DP05_0010PE','DP05_0011PE','DP05_0012PE',
+                 'DP05_0013PE','DP05_0014PE','DP05_0015PE','DP05_0016PE',
+                 'DP05_0017PE']
+    labels = ['GEOID','under 5','5-9','10-14','15-19','20-24','25-34','35-44','45-54',
+              '55-59','60-64','65-74','75-84','85+']
+
+    age_data = data[variables].copy()
+    age_data.columns = labels
+    
+    age_data.iloc[:, 1:] = age_data.iloc[:, 1:].astype(float)
+    
+    
+    return age_data
+
+def extract_race_data(data):
+    
+    variables = ['GEOID','DP05_0037PE','DP05_0038PE','DP05_0039PE','DP05_0045PE',
+                 'DP05_0046PE','DP05_0047PE','DP05_0048PE','DP05_0049PE',
+                 'DP05_0050PE','DP05_0051PE','DP05_0053PE','DP05_0054PE',
+                 'DP05_0055PE','DP05_0056PE','DP05_0057PE','DP05_0059PE',
+                 'DP05_0060PE','DP05_0061PE','DP05_0062PE']
+    
+    labels = ['GEOID','White','Black','American Indian','Asian Indian','Chinese',
+              'Filipino','Japenese','Korean','Vietnamese','Other Asian',
+              'Native Hawaiian','Guamanian or Chamorro','Samoan',
+              'Other Pacific Islander','Some other race',
+              'White and Black or African American',
+              'White and American Indian and Alaska Native','White and Asian',
+              'Black or African American and American Indian and Alaska Native']
+
+    extracted_data = data[variables].copy()
+    extracted_data.columns = labels
+    
+    extracted_data.iloc[:, 1:] = extracted_data.iloc[:, 1:].astype(float)
+    
+    return extracted_data
+    
+
 def plot_choropleth_map(shapefile,data_column,cmap='Blues'):
     
     # Plot the choropleth map
